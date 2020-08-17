926078944f8f9dca53c903f9bc1dfa218f7072d1
andersonmendrot <anderson.mendrot@gmail.com>
2020-08-16 21:47:17 -0300

    Troca de nome de pasta de projeto para SistemaLojaMVC

---
+++ SistemaLojaMVC/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SistemaLojaMVC.Models;
+using SistemaLojaMVC.ViewModels;
+
+namespace SistemaLojaMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            { 
+                PiesOfTheWeek = _pieRepository.PiesOfTheWeek 
+            };
+
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ SistemaLojaMVC/Models/ShoppingCart.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaLojaMVC.Models
+{
+    public class ShoppingCart
+    {
+        private readonly AppDbContext _appDbContext;
+        //private readonly IHttpContextAccessor _httpContextAccessor;
+        private ShoppingCart(AppDbContext appDbContext)//, IHttpContextAccessor httpContextAccessor)
+        {
+            _appDbContext = appDbContext;
+            //_httpContextAccessor = httpContextAccessor;
+        }
+
+        public string ShoppingCartId { get; set; }
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+
+        public static ShoppingCart GetCart(IServiceProvider services)
+        {
+            //session permite que armazenemos informações do lado do servidor (server side)
+            //IHttpContextAccessor acessa as informações da classe HttpContext
+            //HttpContext encapsula toda a informação HTTP específica sobre uma requisição HTTP individual,
+            //como request, response, informações sobre autenticação, etc
+            ISession session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+            //ISession session_alternative = _httpContext.Session;
+            var context = services.GetService<AppDbContext>();
+            string cartId = session.GetString("CartId") ?? Guid.NewGuid().ToString();
+
+            /*
+             * a linha de cima faz a mesma coisa que:
+             *  string cartId;
+                if(session.GetString("CartId") == null)
+                    cartId = Guid.NewGuid().ToString();
+                else cartId = session.GetString("CartId"); 
+             */
+
+            session.SetString("CartId", cartId);
+            return new ShoppingCart(context) { ShoppingCartId = cartId};
+        }
+
+        public void AddToCart(Pie pie)
+        {
+            var shoppingCartItem =
+                    _appDbContext.ShoppingCartItems.SingleOrDefault(
+                        s => s.Pie.PieId == pie.PieId && s.ShoppingCartId == ShoppingCartId);
+
+            if (shoppingCartItem == null)
+            {
+                shoppingCartItem = new ShoppingCartItem
+                {
+                    ShoppingCartId = ShoppingCartId,
+                    Pie = pie,
+                    Amount = 1
+                };
+
+                _appDbContext.ShoppingCartItems.Add(shoppingCartItem);
+            }
+            else
+            {
+                shoppingCartItem.Amount++;
+            }
+            _appDbContext.SaveChanges();
+        }
+
+        public int RemoveFromCart(Pie pie)
+        {
+            var shoppingCartItem =
+                    _appDbContext.ShoppingCartItems.SingleOrDefault(
+                        s => s.Pie.PieId == pie.PieId && s.ShoppingCartId == ShoppingCartId);
+
+            var localAmount = 0;
+
+            if (shoppingCartItem != null)
+            {
+                if (shoppingCartItem.Amount > 1)
+                {
+                    shoppingCartItem.Amount--;
+                    localAmount = shoppingCartItem.Amount;
+                }
+                else
+                {
+                    _appDbContext.ShoppingCartItems.Remove(shoppingCartItem);
+                }
+            }
+
+            _appDbContext.SaveChanges();
+
+            return localAmount;
+        }
+
+        public List<ShoppingCartItem> GetShoppingCartItems()
+        {
+            if (ShoppingCartItems == null)
+            {
+                ShoppingCartItems =
+                       _appDbContext.ShoppingCartItems.Where(c => c.ShoppingCartId == ShoppingCartId)
+                       .Include(x => x.Pie)    
+                       .ToList();
+            }
+
+            return ShoppingCartItems;
+        }
+
+        public void ClearCart()
+        {
+            var cartItems = _appDbContext
+                .ShoppingCartItems
+                .Where(cart => cart.ShoppingCartId == ShoppingCartId);
+
+            _appDbContext.ShoppingCartItems.RemoveRange(cartItems);
+
+            _appDbContext.SaveChanges();
+        }
+
+
+
+        public decimal GetShoppingCartTotal()
+        {
+            var total = _appDbContext.ShoppingCartItems.Where(c => c.ShoppingCartId == ShoppingCartId)
+                .Select(c => c.Pie.Price * c.Amount).Sum();
+            return total;
+        }
+    }
+}
